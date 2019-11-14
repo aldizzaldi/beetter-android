58f858bab6120fdb0be1b8e32ebed9d9bec63548
aldizzaldi <jeflam09@gmail.com>
2019-11-14 22:12:53 +0700

    first commit

--- app/src/main/java/com/example/beetter/MainActivity.java
+++ app/src/main/java/com/example/beetter/MainActivity.java
@@ -4,11 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_navigation);
+
+        PieChart pieChart = findViewById(R.id.piechart);
+        pieChart.setUsePercentValues(true);
+
+        pieChart.setTransparentCircleRadius(20f);
+        pieChart.setHoleRadius(20f);
+
+        pieChart.animateXY(1000,1000);
+
+        List<PieEntry> value = new ArrayList<>();
+        value.add(new PieEntry(60f, "productive"));
+        value.add(new PieEntry(10f, "non productive"));
+        value.add(new PieEntry(30f, "neutral"));
+
+        PieDataSet pieDataSet = new PieDataSet(value,"Status");
+
+        PieData pieData = new PieData(pieDataSet);
+        pieChart.setData(pieData);
+
+        //pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+
+        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
     }
 }
