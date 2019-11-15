0ed26beaeaf6d385919a9301e22bd7f234be72ff
aldizzaldi <jeflam09@gmail.com>
2019-11-15 12:44:00 +0700

    add model view for team feature

--- app/src/main/java/com/example/beetter/ui/team/TeamViewModel.java
+++ app/src/main/java/com/example/beetter/ui/team/TeamViewModel.java
@@ -1,4 +1,19 @@
 package com.example.beetter.ui.team;
 
-public class TeamViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class TeamViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public TeamViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is notifications fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
 }
