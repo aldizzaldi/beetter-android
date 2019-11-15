7055145f07dec7ff9f3eecd1af3385a9969303c7
aldizzaldi <jeflam09@gmail.com>
2019-11-15 12:42:12 +0700

    Fragment Team

--- app/src/main/java/com/example/beetter/ui/team/TeamFragment.java
+++ app/src/main/java/com/example/beetter/ui/team/TeamFragment.java
@@ -1,4 +1,36 @@
 package com.example.beetter.ui.team;
 
-public class TeamFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.beetter.R;
+import com.example.beetter.ui.team.TeamViewModel;
+
+public class TeamFragment extends Fragment {
+
+    private TeamViewModel teamViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        teamViewModel =
+                ViewModelProviders.of(this).get(TeamViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_team, container, false);
+        final TextView textView = root.findViewById(R.id.text_team);
+        teamViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
 }
