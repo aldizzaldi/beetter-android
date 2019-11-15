d4732b3a72574026ea101fa21d99db7118e168a5
aldizzaldi <jeflam09@gmail.com>
2019-11-15 22:21:30 +0700

    try preview some view

--- app/src/main/java/com/example/beetter/MainActivity.java
+++ app/src/main/java/com/example/beetter/MainActivity.java
@@ -18,28 +18,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation);
-
-        PieChart pieChart = findViewById(R.id.piechart);
-        pieChart.setUsePercentValues(true);
-
-        pieChart.setTransparentCircleRadius(20f);
-        pieChart.setHoleRadius(20f);
-
-        pieChart.animateXY(1000,1000);
-
-        List<PieEntry> value = new ArrayList<>();
-        value.add(new PieEntry(60f, "productive"));
-        value.add(new PieEntry(10f, "non productive"));
-        value.add(new PieEntry(30f, "neutral"));
-
-        PieDataSet pieDataSet = new PieDataSet(value,"Status");
-
-        PieData pieData = new PieData(pieDataSet);
-        pieChart.setData(pieData);
-
-        //pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
-
-        pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+        setContentView(R.layout.daily_report);
     }
 }
