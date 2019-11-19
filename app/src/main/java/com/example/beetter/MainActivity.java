fa6e1b42062d1e9d63d645bb18d2a2557c0fd1a6
wifda <wifdamu@gmail.com>
2019-11-19 11:31:26 +0700

    menambahkan toolbar 2

--- app/src/main/java/com/example/beetter/MainActivity.java
+++ app/src/main/java/com/example/beetter/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.beetter;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.github.mikephil.charting.charts.PieChart;
 import com.github.mikephil.charting.data.PieData;
@@ -18,6 +21,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.daily_report);
+//        setContentView(R.layout.daily_report);
+        setContentView(R.layout.activity_main);
+
+//        ActionBar actionBar = getSupportActionBar(); // or getActionBar();
+//        getSupportActionBar().setTitle("My new title"); // set the top title
+//        String title = actionBar.getTitle().toString(); // get the title
+//        actionBar.hide(); // or even hide the actionbar
+//
+//        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+//        getSupportActionBar().setDisplayUseLogoEnabled(true);
+//
+////        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
