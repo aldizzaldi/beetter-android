9f4dadfadabc3f0e22363984623f06df5200a5e6
sherlymaya <57797808+sherlymaya@users.noreply.github.com>
2019-11-21 16:51:28 +0700

    SlidingUp panel

---
+++ app/src/main/java/com/example/beetter/SlidingUp.java
@@ -0,0 +1,51 @@
+package com.example.beetter;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+public class SlidingUp extends BottomSheetDialogFragment {
+    private SlidingUpListener mListener;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.team, container, false);
+
+        Button btnExitTeam = v.findViewById(R.id.btnExitTeam);
+
+        btnExitTeam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return v;
+    }
+
+    public interface SlidingUpListener {
+        void onClicked(String text);
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            mListener = (SlidingUpListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+            + " must implemet Sliding Up Listener");
+        }
+
+    }
+}
